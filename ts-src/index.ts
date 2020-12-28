555f890700daced16267a4ff208fbf8f82c138f9
Zakary Nichols <zakaryn@VTDEV-iMac.local>
2020-12-28 08:20:37 -0500

    learning dom interfaces

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -6,10 +6,9 @@ const greeter = (person: string) => {
     return `Hello ${person}!`;
 };
 
-const greet = greeter('Jane');
+const greet = greeter('Zak');
 
 document.body.textContent = greet;
-
 interface Person {
     firstName: string,
     lastName: string
@@ -18,3 +17,23 @@ interface Person {
 const sayHello = ({ firstName, lastName }: Person) => `Hello ${firstName} ${lastName}`;
 
 sayHello({ firstName: 'Jack', lastName: 'William' });
+
+const app = document.createElement("div");
+
+app.setAttribute('id', 'app');
+
+const p = document.createElement('p');
+p.setAttribute('class', 'woo');
+
+app?.appendChild(p);
+
+p.textContent = 'Hello world!';
+
+document.body.appendChild(app);
+
+const test = document.getElementById('app');
+
+console.log('test', test);
+
+const q = document.querySelector('.woo');
+console.log(q)
